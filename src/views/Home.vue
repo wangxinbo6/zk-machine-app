aa1fef8e81932869ecaf5958f2238c0a97453e21
wangxinbo6 <83815101@qq.com>
2020-06-18 10:44:14 +0800

    home页面改动

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,52 +1,35 @@
 <template>
   <div class="home">
-    <div>
-      <b-carousel
-        id="carousel-1"
-        v-model="slide"
-        :interval="4000"
-        controls
-        indicators
-        background="#ababab"
-        img-width="1024"
-        img-height="300"
-        style="text-shadow: 1px 1px 2px #333;"
-        @sliding-start="onSlideStart"
-        @sliding-end="onSlideEnd"
-      >
-        <!-- Text slides with image -->
-        <b-carousel-slide
-          caption="First slide"
-          style="max-height:50vh"
-          
-          text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-          img-src="https://picsum.photos/1024/480/?image=52"
-          
-        ></b-carousel-slide>
-
-        <!-- Slides with image only -->
-        <b-carousel-slide style="max-height:50vh" img-src="https://picsum.photos/1024/480/?image=52"></b-carousel-slide>
-
-      </b-carousel>
-
-    </div>
-    <div style="width:94% ;margin:0 auto">
-      <div class="title">
-        家庭药箱
-      </div>
-      <b-container fluid>
+    
+    <div style="width:94% ;margin:0 auto" class="homeContent">
+      <b-container style="padding-top:30px">
+        <div class="title">
+          <p></p>
+          <p>家庭药箱</p>
+          <p></p>
+          <div class="clear"></div>
+        </div>
         <b-row style="padding:12px" >
-          <b-col  gutter cols="6" sm="6" xs="6" md="4" lg="3" xl="2" v-for="(item,index) in shopList" :key="index">
+          <b-col  gutter cols="12" sm="12" xs="6" md="4" lg="3" xl="3" v-for="(item,index) in shopList" :key="index" class="contents">
             <div class="tl card" style="margin-bottom:6px">
-              <b-img :src="item.url" fluid="6" alt="Responsive image"></b-img>
-              <p style="text-align:center">{{item.title}}</p>
-              <p>库存：{{item.num}} 台</p>
-              <p>售价：￥{{item.money}}</p>
+              <b-row style="padding:0;" >
+                <b-col cols="4" sm="5" xs="12" md="12" lg="12" xl="12">
+                  <b-img :src="item.url" fluid-grow alt="Responsive image" class="img"></b-img>
+                </b-col>
+                <b-col cols="8" sm="7" xs="12" md="12" lg="12" xl="12" class="shopText">
+                  <p style="margin:10px;font-size:12px;height: 36px;" class="shopTextTitle">{{item.title}}</p>
+                  <p style="margin:10px;color: #F41;"><i style="font-size:12px">￥</i>{{item.money}}</p>
+                  <p style="margin:10px;font-size:12px">库存：{{item.num}} 台</p>
+                
+                  <b-button size="sm" variant="success">立即抢购</b-button>
+
+                </b-col>
+              </b-row>
             </div>
           </b-col>
         </b-row>
       </b-container>
-      <div class="title">
+      <!-- <div class="title">
         饮料机
       </div>
       <b-container fluid>
@@ -60,7 +43,7 @@
             </div>
           </b-col>
         </b-row>
-      </b-container>
+      </b-container> -->
     </div>
   </div>
 </template>
@@ -76,20 +59,20 @@ export default {
       slide: 0,
       sliding: null,
       shopList:[
-        {url:"http://img2.imgtn.bdimg.com/it/u=111399773,3729640683&fm=26&gp=0.jpg",title:"西瓜机",num:"999",money:8700},
-        {url:"http://img2.imgtn.bdimg.com/it/u=111399773,3729640683&fm=26&gp=0.jpg",title:"葡萄机",num:"10",money:96},
-        {url:"http://img2.imgtn.bdimg.com/it/u=111399773,3729640683&fm=26&gp=0.jpg",title:"葡萄机",num:"10",money:96},
-        {url:"http://img2.imgtn.bdimg.com/it/u=111399773,3729640683&fm=26&gp=0.jpg",title:"葡萄机",num:"10",money:96},
-        {url:"http://img2.imgtn.bdimg.com/it/u=111399773,3729640683&fm=26&gp=0.jpg",title:"葡萄机",num:"10",money:96},
-        {url:"http://img2.imgtn.bdimg.com/it/u=111399773,3729640683&fm=26&gp=0.jpg",title:"葡萄机",num:"10",money:96}
+        {url:"https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2574012528,4184940&fm=26&gp=0.jpg",title:"西瓜机西瓜机西瓜机西瓜机西瓜机西瓜机西瓜机西瓜机西瓜机西瓜机西瓜机西瓜机西瓜机西瓜机西瓜机西瓜机西瓜机西瓜机西瓜机西瓜机西瓜机西瓜机西瓜机西瓜机",num:"999",money:8700},
+        {url:"https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2574012528,4184940&fm=26&gp=0.jpg",title:"葡萄机",num:"10",money:96},
+        {url:"https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2574012528,4184940&fm=26&gp=0.jpg",title:"葡萄机",num:"10",money:96},
+        {url:"https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2574012528,4184940&fm=26&gp=0.jpg",title:"葡萄机",num:"10",money:96},
+        {url:"https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2574012528,4184940&fm=26&gp=0.jpg",title:"葡萄机",num:"10",money:96},
+        {url:"https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2574012528,4184940&fm=26&gp=0.jpg",title:"葡萄机",num:"10",money:96}
       ],
       shopList2:[
-        {url:"http://img2.imgtn.bdimg.com/it/u=111399773,3729640683&fm=26&gp=0.jpg",title:"西瓜机",num:"999",money:8700},
-        {url:"http://img2.imgtn.bdimg.com/it/u=111399773,3729640683&fm=26&gp=0.jpg",title:"葡萄机",num:"10",money:96},
-        {url:"http://img2.imgtn.bdimg.com/it/u=111399773,3729640683&fm=26&gp=0.jpg",title:"葡萄机",num:"10",money:96},
-        {url:"http://img2.imgtn.bdimg.com/it/u=111399773,3729640683&fm=26&gp=0.jpg",title:"葡萄机",num:"10",money:96},
-        {url:"http://img2.imgtn.bdimg.com/it/u=111399773,3729640683&fm=26&gp=0.jpg",title:"葡萄机",num:"10",money:96},
-        {url:"http://img2.imgtn.bdimg.com/it/u=111399773,3729640683&fm=26&gp=0.jpg",title:"葡萄机",num:"10",money:96}
+        {url:"https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2574012528,4184940&fm=26&gp=0.jpg",title:"西瓜机",num:"999",money:8700},
+        {url:"https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2574012528,4184940&fm=26&gp=0.jpg",title:"葡萄机",num:"10",money:96},
+        {url:"https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2574012528,4184940&fm=26&gp=0.jpg",title:"葡萄机",num:"10",money:96},
+        {url:"https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2574012528,4184940&fm=26&gp=0.jpg",title:"葡萄机",num:"10",money:96},
+        {url:"https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2574012528,4184940&fm=26&gp=0.jpg",title:"葡萄机",num:"10",money:96},
+        {url:"https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2574012528,4184940&fm=26&gp=0.jpg",title:"葡萄机",num:"10",money:96}
       ]
     }
   },
@@ -108,18 +91,85 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  .home{
+    background-color: #fafafa;
+  }
   .title{
     width: 100%;
     margin: 0 auto;
-    margin-top: 30px;
-    margin-bottom: 30px;
-    padding: 10px 0;
-    text-align: left;
-    border-bottom: 2px solid rgba(128, 128, 128, 0.39);
-    font-size: 24px;
+    padding: 12px;
+    text-align: center;
+    // border-bottom: 2px solid rgba(128, 128, 128, 0.39);
+    font-size: 18px;
     font-weight: bold;
+    P{
+      display: inline-block;
+      width: 33.3333333%;
+      float: left;
+      &:nth-child(1){
+        border-bottom: 1px solid #ccc;
+        height: 10px;
+      }
+      &:nth-child(3){
+        border-bottom: 1px solid #ccc;
+        height: 10px;
+      }
+    }
   }
-  .card>p{
-    padding:4px 10px;
+  .card{
+    border: none;
+    &>p{
+      padding:4px 10px;
+    }
+  }
+  .shopText{
+    letter-spacing: 0.8px;
+    font-size: 18px;
+    color: #3d3d3d;
+    position: relative;
+    .shopTextTitle{
+      text-overflow: -o-ellipsis-lastline;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      display: -webkit-box;
+      -webkit-line-clamp: 2;
+      line-clamp: 2;
+      -webkit-box-orient: vertical;
+    }
+    button{
+      position: absolute;
+      top: 56px;
+      right: 25px;
+      border-radius: 30px;
+      background-color: #ffeded;
+      border: 1px solid #FF0036;
+      color: #FF0036;
+    }
+  }
+  @media (max-width: 1200px) { 
+    .row{
+      padding: 0!important;
+      margin: 0!important;
+    }
+    .col{
+      padding: 0!important;
+    }
+    .container{
+      padding: 0!important;;
+    }
+    .img{
+      height: 100%;
+      padding: 14px 0;
+    }
+    .shopText{
+      padding-left: 0!important;
+      margin-left: -10px;
+    }
+    .homeContent{
+      width: 100%;
+    }
+    .contents{
+      padding: 0 !important;
+    }
   }
 </style>
